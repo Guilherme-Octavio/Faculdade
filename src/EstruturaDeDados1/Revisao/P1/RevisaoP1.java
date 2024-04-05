0636e0b09785113d4b38abdaa819efa38ba8e2f2
Guilherme Octávio <guilhermeoctavio01@gmail.com>
2024-04-04 21:53:53 -0300

    Revisão Estrutura de dados
    Revisão da primeira prova de Estrutura de Dados

---
+++ src/EstruturaDeDados1/Revisao/P1/RevisaoP1.java
@@ -0,0 +1,69 @@
+package EstruturaDeDados1.Revisao.P1;
+
+import javax.xml.xpath.XPathExpressionException;
+import java.lang.reflect.AnnotatedArrayType;
+
+public class RevisaoP1 {
+
+    public static void main(String[] args) {
+        // Resolução - Exec 2
+        System.out.println("Exec2: " +calculaExponenciacao(2, 2));
+
+        //Resolução - Exec 3
+        Aluno[] pessoa = new Aluno[4];
+        pessoa[0] = new Aluno("Guilherme", 18);
+        pessoa[1] = new Aluno("Andre", 21);
+        pessoa[2] = new Aluno("Augusto", 18);
+        pessoa[3] = new Aluno("Cristian", 30);
+
+
+        System.out.print("Exec3: ");
+        imprimeNomes(pessoa);
+        System.out.println();
+
+        assert pegaAlunoIdade(pessoa) != null;
+        System.out.println("Exec4: "+pegaAlunoIdade(pessoa).nome);
+
+    }
+
+    // Exec 2
+    static int calculaExponenciacao(int base, int expoente){
+        int res = 0;
+        if (expoente == 0){
+            return 1;
+        }
+        return base * calculaExponenciacao(base, expoente - 1);
+    }
+
+    // Exec 3
+    public static class Aluno{
+        String nome;
+        int idade;
+
+        Aluno(String nome, int idade){
+            this.nome  = nome;
+            this.idade = idade;
+        }
+    }
+
+    static void imprimeNomes(Aluno[] dados){
+        for(Aluno p: dados){
+            if (p.idade > 18){
+                System.out.print(p.nome+' ');
+            }
+        }
+
+    }
+
+    // Exec 4
+
+    public static Aluno pegaAlunoIdade(Aluno[] dados){
+        for (Aluno p: dados){
+            if (p.idade == 30){
+                return p;
+            }
+        }
+        return null;
+    }
+}
+
